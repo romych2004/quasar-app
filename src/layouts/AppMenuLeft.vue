2f9fd5712ace15dbe568edf2eb6e58344fc8dfdc
Roman <romych2004@list.ru>
2018-05-28 10:03:03 +0300

    dev

--- src/layouts/AppMenuLeft.vue
+++ src/layouts/AppMenuLeft.vue
@@ -4,14 +4,14 @@
       :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null">
 		<q-list
  			no-border
-        	link
-        	inset-delimiter>
+      link
+      inset-delimiter>
 			<q-item v-for="item in items" :key="item.id" :to="item.href" exact>
 				<q-item-side icon="school" />
 				<q-item-main :label="item.label" />
 			</q-item>
-      	</q-list>
-    </q-layout-drawer>
+    </q-list>
+  </q-layout-drawer>
 </template>
 
 <script>
